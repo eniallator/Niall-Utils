b3ad242a554dbc538f116a93d5ed6aa92e7e2d3d
eniallator <eniallatormc@gmail.com>
2026-06-01 19:16:23 +0000

    Made tagged.ts better

--- src/core/tagged.ts
+++ src/core/tagged.ts
@@ -1,5 +1,9 @@
-export type Tagged<T, N extends string> = T & { _tag_: N };
-export type Untag<T extends Tagged<unknown, string>> = Omit<T, "_tag_">;
+declare const Tag: unique symbol;
+
+export type Tagged<Type, Name extends string> = Type & {
+  [Tag]: { readonly type: Type; readonly name: Name };
+};
+export type Untag<T extends Tagged<unknown, string>> = T[typeof Tag]["type"];
 
 export const unsafeTag =
   <T extends Tagged<unknown, string>>() =>
