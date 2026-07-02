1aa1c2db696aee4ee568f4870705a84d392c8c10
eniallator <eniallatormc@gmail.com>
2026-07-02 10:13:37 +0000

    Added a ci action

--- vitest.config.js
+++ vitest.config.js
@@ -1,23 +1,35 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig, mergeConfig } from "vitest/config";
+import process from "node:process";
 
-export default defineConfig({
+const baseConfig = defineConfig({
   test: {
     watch: false,
     isolate: true,
     passWithNoTests: true,
     environment: "jsdom",
     coverage: {
+      statements: 100,
+      functions: 100,
+      branches: 100,
+      lines: 100,
+      perFile: true,
+      autoUpdate: true,
       enabled: true,
       reporter: ["text-summary"],
       include: ["src/**/*"],
-      thresholds: {
-        statements: 100,
-        functions: 100,
-        branches: 100,
-        lines: 100,
-        perFile: true,
-        autoUpdate: true,
-      },
     },
   },
 });
+
+const ciConfig = defineConfig({
+  test: {
+    maxWorkers: 1,
+    minWorkers: 1,
+    fileParallelism: false,
+  },
+});
+
+export default mergeConfig(
+  baseConfig,
+  process.env.NODE_ENV === "ci" ? ciConfig : {}
+);
